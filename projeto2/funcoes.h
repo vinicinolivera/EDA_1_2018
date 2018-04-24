38ff6b362b65c94724522f40c0e5030241b75a06
Vinicius Rodrigues <vinifladf@gmail.com>
2018-04-24 09:30:36 -0300

    Pegando quantidade de colunas e linhas do arquivo
    Signed-off-by: Vinicius Rodrigues <vinifladf@gmail.com>

--- projeto2/funcoes.h
+++ projeto2/funcoes.h
@@ -1,4 +1,28 @@
 #ifndef funcoes_h
 #define funcoes_h
-#endif
 
+
+int **aloca_memoria_arquivo(int num_linhas, int num_colunas){
+	int **matriz;
+	int i = 0;
+
+	//alocando memoria para as linhas
+	matriz = (int **) calloc(num_linhas, sizeof(int *));
+	if(matriz == NULL){
+		printf ("** Erro: Memoria Insuficiente **");
+		return (NULL);
+	}
+
+	//alocando memoria para num_colunas
+	for(i = 0; i < num_linhas; i++){
+		matriz[i] = (int *) calloc(num_colunas, sizeof(int *));
+		if(matriz[i] == NULL){
+			printf ("** Erro: Memoria Insuficiente **");
+			return (NULL);
+		}
+	}
+	return (matriz);
+
+}
+
+#endif
--- projeto2/projeto2.c
+++ projeto2/projeto2.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "funcoes.h"
 
+int **aloca_memoria_arquivo(int num_linhas, int num_colunas);
 int main()
 {
-	char url[]="DataSet/asphalt/asphalt_01.txt";
+	char url[]="DataSet/asphalt/asphalt_05.txt";
 	char ch;
-	int num_linhas=0;
+	int num_linhas=0, num_colunas=0;
+	char *token;
+	int **matriz;
 	FILE *arq;
-	
+
 	arq = fopen(url, "r");
-	if(arq == NULL)
-			printf("Erro, nao foi possivel abrir o arquivo\n");
-	else
+	if(arq == NULL){
+		printf("Erro, nao foi possivel abrir o arquivo\n");
+	}
+
+	else{
+		while( (ch=fgetc(arq))!= EOF ){
+			if(ch == '\n'){
+				num_linhas++;
+			}
+			else{
+				if(num_linhas == 9){
+					if(ch == ';'){
+						num_colunas++;
+					}
+				}
+			}
+		}
+	}
+
+	printf("Existem %d linhas no arquivo\n", num_linhas);
+	printf("Existem %d colunas no arquivo\n", num_colunas);
+
+	matriz = aloca_memoria_arquivo(num_linhas, num_colunas);
 
 
   return 0;
